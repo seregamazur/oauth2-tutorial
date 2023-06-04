d968887c99e6d80cad16d54df429bc7ef0acae31
Serhii Mazur <serhiy.mazur0@gmail.com>
2023-06-04 15:07:55 +0200

    Save App files.

---
+++ oauth2-tutorial-ui/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage1 from './login/LoginPage';
+
+function App() {
+  return (
+      <div className="App">
+        <LoginPage1 />
+      </div>
+  );
+}
+
+export default App;

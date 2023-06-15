2f19b56f6cd662293eda963ccfd67dfba12eb6f6
Serhii Mazur <serhiy.mazur0@gmail.com>
2023-06-15 21:59:43 +0200

    OAUT-2. Add liquibase. Add fields to User entity.

--- src/main/java/com/seregamazur/oauth2/tutorial/crud/User.java
+++ src/main/java/com/seregamazur/oauth2/tutorial/crud/User.java
@@ -1,5 +1,7 @@
 package com.seregamazur.oauth2.tutorial.crud;
 
+import java.time.Instant;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,7 +12,12 @@ import lombok.Data;
 public class User {
     @Id
     private String id;
-    private String fullName;
+    private String login;
+    private String firstName;
+    private String lastName;
+    private String imageUrl;
     private String email;
-
+    private String password;
+    private Instant createdDate;
+    private Instant lastModifiedDate;
 }

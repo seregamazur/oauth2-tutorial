f11323b53fbb06f1309da53cee10ee0e35a169a2
Serhii Mazur <serhiy.mazur0@gmail.com>
2023-06-23 23:05:28 +0200

    OAUT-5. Add changes to Facebook.

---
+++ src/main/java/com/seregamazur/oauth2/tutorial/service/FacebookService.java
@@ -0,0 +1,47 @@
+package com.seregamazur.oauth2.tutorial.service;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.seregamazur.oauth2.tutorial.client.model.token.OAuth2AccessToken;
+import com.seregamazur.oauth2.tutorial.crud.User;
+import com.seregamazur.oauth2.tutorial.crud.UserRepository;
+import com.seregamazur.oauth2.tutorial.security.jwt.AccessTokenVerificationException;
+import com.seregamazur.oauth2.tutorial.security.jwt.JWTToken;
+import com.seregamazur.oauth2.tutorial.security.jwt.TokenProvider;
+
+import static com.seregamazur.oauth2.tutorial.service.JWTVerificationService.verifyToken;
+
+@Service
+public class FacebookService {
+
+    private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
+    private final GoogleIdTokenVerifier googleIdTokenVerifier;
+
+    public FacebookService(UserRepository userRepository,
+        TokenProvider tokenProvider, GoogleIdTokenVerifier googleIdTokenVerifier) {
+        this.userRepository = userRepository;
+        this.tokenProvider = tokenProvider;
+        this.googleIdTokenVerifier = googleIdTokenVerifier;
+    }
+
+    public JWTToken createJwtFromAccessToken(OAuth2AccessToken oAuth2AccessToken) {
+        verifyToken(oAuth2AccessToken.getIdTokenValue());
+        GoogleIdToken idToken;
+        try {
+            idToken = googleIdTokenVerifier.verify(oAuth2AccessToken.getIdTokenValue());
+        } catch (Exception e) {
+            throw new AccessTokenVerificationException(e);
+        }
+        GoogleIdToken.Payload payload = idToken.getPayload();
+        User user = userRepository.findByEmail(payload.getEmail())
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        String jwt = tokenProvider.createToken(user, oAuth2AccessToken.getIdTokenValue(),
+            oAuth2AccessToken.getScope(), "facebook", true);
+        return new JWTToken(jwt);
+    }
+
+}
